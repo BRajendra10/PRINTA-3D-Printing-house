9720521c63274988a4b061425e5d07b30089c25c
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-27 13:51:35 +0530

    adding animation for badge that every section has

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const info_eliment = document.querySelectorAll(".info-eliment");
 const main_sec_info = document.querySelectorAll(".main-sec-info");
 const header_section_eli = document.querySelectorAll(".header-section-el");
+const section_badge = document.querySelectorAll("#section-badge");
 
 function slideFromLeft(lists) {
-    const observer = new IntersectionObserver((entries) => {
+    const leftObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 gsap.from(entry.target,
                     { x: -300, duration: 1.5, ease: "back.out()" }
                 )
 
-                observer.unobserve(entry.target);
+                leftObserver.unobserve(entry.target);
             }
         })
     })
 
     lists.forEach((el, i) => {
-        observer.observe(el);
+        leftObserver.observe(el);
+    })
+}
+
+function slideFromTop(lists){
+    const topObserve = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting){
+                gsap.from(entry.target,
+                    {top: "-5%", duration: 0.8}
+                )
+
+                topObserve.unobserve(entry.target);
+            }
+        })
+    })
+
+    lists.forEach((el) => {
+        topObserve.observe(el);
     })
 }
 
 slideFromLeft(info_eliment);
 slideFromLeft(main_sec_info);
+
+slideFromTop(section_badge);
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
